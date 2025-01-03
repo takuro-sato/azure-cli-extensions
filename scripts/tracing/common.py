2775b8afdb79fb6ec76d65497555ecdd30121f26
maowtm <m@maowtm.org>
2025-01-03 13:59:39 +0000

    Make tracing code retry, in the event of e.g. 429 TooManyRequests (#161)

--- scripts/tracing/common.py
+++ scripts/tracing/common.py
@@ -2,6 +2,9 @@ from azure.kusto.data import KustoClient, KustoConnectionStringBuilder
 import os
 from datetime import datetime
 import json
+import random
+import time
+import sys
 
 
 def get_env_or_die(env_name):
@@ -47,12 +50,25 @@ def trace_step(
         "Error": error,
         "Output": output,
     }
-
-    client = KustoClient(conn)
     query_str = f".ingest inline into table {KUSTO_TABLE} with (format='json') <|\n  "
     query_str += json.dumps(trace_obj)
     query_str += "\n"
-    client.execute(KUSTO_DATABASE, query_str)
+    nb_attempts = 0
+    max_attempts = 3
+    while True:
+        try:
+            client = KustoClient(conn)
+            client.execute(KUSTO_DATABASE, query_str)
+            break
+        except Exception as e:
+            nb_attempts += 1
+            if nb_attempts >= max_attempts:
+                raise
+            else:
+                print(f"Failed to trace step {step_name}: {e}", file=sys.stderr)
+                wait_time = random.randint(10, 30)
+                print(f"Retrying in {wait_time} seconds...", file=sys.stderr, flush=True)
+                time.sleep(wait_time)
 
     print(
         f"Traced {trace_obj['DeploymentName']}/{trace_obj['StepName']} @ {trace_obj['Timestamp']}: {trace_obj['Status']}" +
