6d0b22f017eda6cab392ef4c1f22bfc362a8cc11
maowtm <m@maowtm.org>
2025-01-07 14:56:11 +0000

    Bump confcom to 1.2.0 (and use it in all tests including fragments) (#165)

--- scripts/init_run.sh
+++ scripts/init_run.sh
@@ -12,7 +12,7 @@ echo Install c_aci_testing package
 ./scripts/install-c-aci-testing.sh
 
 echo Set Confcom Version
-./scripts/install-confcom.sh 1.1.1
+./scripts/install-confcom.sh 1.2.0
 
 echo Setup Docker
 sudo usermod -aG docker $USER
