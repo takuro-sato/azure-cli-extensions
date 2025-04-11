8f5f46f66f029bd7aae3ebd5c365c8ae6782ed4d
Dominic Ayre <dominicayre@microsoft.com>
2025-04-11 17:11:58 +0100

    Add workaround to install bicep in required location (#215)

--- scripts/install-c-aci-testing.sh
+++ scripts/install-c-aci-testing.sh
@@ -1,44 +1,43 @@
 #!/bin/bash
 
-# Temporary fix for workflow failing due to error with missing bicep binary
-# -------------------------------------------------------------------------
-#
-#  This file is using a custom branch of c-aci-testing while we figure out
-#  what is the actual cause of the issue. See
-#  https://github.com/microsoft/confidential-aci-dashboard/pull/214 for details.
-#
+version="1.1.2"
+tgz_url="https://github.com/microsoft/confidential-aci-testing/releases/download/$version/c_aci_testing-$version.tar.gz"
+tgz_name="c-aci-testing.tar.gz"
 
+attempts=0
+while :; do
+    echo "Downloading $tgz_url"
+    curl -sL --fail -o "$tgz_name" "$tgz_url"
+    if [ $? -eq 0 ]; then
+        break
+    else
+        echo "Download failed."
+        attempts=$((attempts + 1))
+        if [ $attempts -ge 3 ]; then
+            echo "Failed to download after 3 attempts."
+            exit 1
+        fi
+        echo Retrying in 5s
+        sleep 5
+    fi
+done
 
-# version="1.1.2"
-# tgz_url="https://github.com/microsoft/confidential-aci-testing/releases/download/$version/c_aci_testing-$version.tar.gz"
-# tgz_name="c-aci-testing.tar.gz"
+set -e
 
-# attempts=0
-# while :; do
-#     echo "Downloading $tgz_url"
-#     curl -sL --fail -o "$tgz_name" "$tgz_url"
-#     if [ $? -eq 0 ]; then
-#         break
-#     else
-#         echo "Download failed."
-#         attempts=$((attempts + 1))
-#         if [ $attempts -ge 3 ]; then
-#             echo "Failed to download after 3 attempts."
-#             exit 1
-#         fi
-#         echo Retrying in 5s
-#         sleep 5
-#     fi
-# done
+pip install "$tgz_name"
+rm "$tgz_name"
 
-# set -e
-
-# pip install "$tgz_name"
-# rm "$tgz_name"
+# Workaround for a bug where bicep isn't in the expected location
+if [ ! -f ~/.azure/bin/bicep ]; then
+    mkdir -p ~/.azure/bin/
+    curl -Lo ~/.azure/bin/bicep \
+        https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64
+    chmod +x ~/.azure/bin/bicep
+fi
 
 # Uncomment for private branch testing
-BRANCH="tingmao/deploy-with-arm"
-git clone 'https://github.com/microsoft/confidential-aci-testing.git' --branch $BRANCH /tmp/c-aci-testing
-cd /tmp/c-aci-testing
-pip install flit
-flit install
+# BRANCH=???
+# git clone 'https://github.com/microsoft/confidential-aci-testing.git' --branch $BRANCH /tmp/c-aci-testing
+# cd /tmp/c-aci-testing
+# pip install flit
+# flit install
