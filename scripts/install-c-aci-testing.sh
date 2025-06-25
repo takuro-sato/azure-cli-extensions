8d51bc2e262805c2ddf6fc789e0f2f40a138c8e6
Tingmao Wang <tingmaowang@microsoft.com>
2025-06-25 16:41:57 +0100

    Bump c-aci-testing to 1.2.4

--- scripts/install-c-aci-testing.sh
+++ scripts/install-c-aci-testing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="1.2.3"
+version="1.2.4"
 tgz_url="https://github.com/microsoft/confidential-aci-testing/releases/download/$version/c_aci_testing-$version.tar.gz"
 tgz_name="c-aci-testing.tar.gz"
 
