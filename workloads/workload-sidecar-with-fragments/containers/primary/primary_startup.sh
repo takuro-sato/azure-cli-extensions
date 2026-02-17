7e2cb5c3c3549f406827376d70c9cda08fac5933
Tingmao Wang <m@maowtm.org>
2026-02-17 14:10:29 +0000

    Increase reliability of fragment test (#316)

--- workloads/workload-sidecar-with-fragments/containers/primary/primary_startup.sh
+++ workloads/workload-sidecar-with-fragments/containers/primary/primary_startup.sh
@@ -10,5 +10,18 @@ echo Reference info SHA256SUM: $(base64 -d < /security-context-*/reference-info-
 
 sleep 5
 # Attempt to curl server in sidecar
-curl --fail-with-body 'http://127.0.0.1:8000'
+for i in $(seq 1 10); do
+    if curl --fail-with-body 'http://127.0.0.1:8000'; then
+        echo "curl sidecar succeeded"
+        break
+    fi
+    if [ $i -lt 9 ]; then
+        echo "Attempt $i failed, retrying in 5 seconds..."
+        sleep 5
+    else
+        echo "All 3 attempts failed"
+        echo "ERROR: failed to curl sidecar"
+        sleep infinity
+    fi
+done
 sleep infinity
