c51ec8035c2ffb1dd8c71994997268bfdde4cb16
Tingmao Wang <tingmaowang@microsoft.com>
2026-03-10 00:03:22 +0000

    aci_deploy_traced.sh: Continue deployment even if it exceeds 20 mins, and trace correlationId even in timeout case

--- scripts/aci_deploy_traced.sh
+++ scripts/aci_deploy_traced.sh
@@ -7,22 +7,34 @@ if [ -z "$DEPLOYMENT_NAME" ]; then
   exit 1
 fi
 
+MAX_DURATION_MS=1200000
+
 script_dir="$(dirname "$0")"
 
 args=("$@")
 $script_dir/tracing/trace_step.py --start "aci deploy $DEPLOYMENT_NAME"
+out_file=`mktemp`
 err_file=`mktemp`
-timeout -s INT 20m c-aci-testing aci deploy "${args[@]}" 2> "$err_file"
+c-aci-testing aci deploy "${args[@]}" --timeout 3600 --deploy-output-file "$out_file" 2> "$err_file"
 status=$?
+cat "$out_file"
+out_file_is_valid=$(jq -r '.' < "$out_file" > /dev/null 2>&1 && echo "true" || echo "false")
 cat "$err_file" >&2
 if [ $status -ne 0 ]; then
-  if [ $status -eq 124 ]; then
-    echo "Deployment timed out"
-    $script_dir/tracing/trace_step.py --complete --err "Deployment timed out after 20m" --strict
+  err_str=""
+  if [[ "$out_file_is_valid" == "true" ]]; then
+    err_str="$(jq -r '.error' < "$out_file") (correlation ID: $(jq -r '.correlationId' < "$out_file"))"
   else
-    $script_dir/tracing/trace_step.py --complete --err "$(printf "Failed to deploy to %s:\n%s" "$DEPLOYMENT_NAME" "$(cat "$err_file")")" --strict
+    err_str="$(cat "$err_file")"
   fi
+  $script_dir/tracing/trace_step.py --complete --err "$(printf "Failed to deploy to %s:\n%s" "$DEPLOYMENT_NAME" "$err_str")" --strict --output-from-stdin < "$out_file"
 else
-  $script_dir/tracing/trace_step.py --complete --strict
+  durationMs=$(jq -r '.durationMs' < "$out_file")
+  if [ "$durationMs" -le "$MAX_DURATION_MS" ]; then
+    $script_dir/tracing/trace_step.py --complete --strict --output-from-stdin < "$out_file"
+  else
+    $script_dir/tracing/trace_step.py --complete --err "Deployment succeed, but exceeded $((MAX_DURATION_MS / 1000 / 60)) minutes (took $((durationMs / 1000 / 60)) minutes)" --strict --output-from-stdin < "$out_file"
+  fi
 fi
+rm "$out_file" "$err_file"
 exit $status
--- scripts/init_run.sh
+++ scripts/init_run.sh
@@ -7,7 +7,7 @@ az config set core.collect_telemetry=false --only-show-errors
 
 export CONFCOM_VERSION="1.4.5"
 export C_ACI_TESTING_VERSION="1.2.15"
-export C_ACI_TESTING_BRANCH="vm-gen-script"
+export C_ACI_TESTING_BRANCH="deploy-timeout"
 
 if [ -n "$INIT_RUN_POPULATE_CACHE" ]; then
   if ! ./scripts/venv/get-deps.sh; then
